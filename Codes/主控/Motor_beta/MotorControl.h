12ba3371a2027ea2d474048959e6a7cf23aba06f
Wenhao <815128388@qq.com>
2016-05-03 22:16:37 +0800

    Finish a simple PID controller

--- Codes/主控/Motor_beta/MotorControl.h
+++ Codes/主控/Motor_beta/MotorControl.h
@@ -14,7 +14,7 @@
 *      speed: the speed of the motor after turning
 *
 *  The angle we get from the gyroscope is from -180 to 180, but in order to simplify the
-*  adjustion, we invert the scope from 0 to 360, clockwise represents turning right
+*  adjustion, we convert the scope from 0 to 360, clockwise represents turning right
 
 ****************************************************************************************/
 #ifndef MOTOR_CONTROL
@@ -23,6 +23,7 @@
 // some constrain
 #define MAX_SPEED 100   
 #define MAX_ANGLE 360
+#define MIN_ANGLE 0
 #define LEFT 0
 #define RIGHT 1
 
@@ -30,8 +31,10 @@
 #define P_DATA 10
 #define I_DATA 0.6 
 #define D_DATA 1
+#define ACCEPTTED_ERROR 10
 
 int16_t constrain(int16_t input, int16_t min, int16_t max);
+int16_t angleTransform(int16_t angle_ABS180);
 void Moter_Move(uint16_t angle, bool if_related, uint16_t speed);
 
 #endif // MOTOR_CONTROL
