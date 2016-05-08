690d312cba58de4f68426dc078f617c22b87907a
Wenhao <815128388@qq.com>
2016-05-08 15:49:09 +0800

    change PID controller

--- Codes/V3-Driver Board Controller/PID_Controller/MotorControl.h
+++ Codes/V3-Driver Board Controller/PID_Controller/MotorControl.h
@@ -1,9 +1,10 @@
 /***************************************************************************************
-
+*
 *  Used functions:
 *  Motor_If_Forward(left_or_right, if_forward)       decide the direction of the motor
 *  Motor_Set_Speed(left_or_right, speed)             set the speed
 *  Inertia_Get_Angle_Yaw()  get the angle from the gyroscope(double)
+*  Get_Speed(left_or_right) get speed number (0 ~ 1000) 
 *  
 *  Output function:
 *  Moter_Move(uint16_t angle, bool if_related, uint16_t speed)
@@ -15,26 +16,41 @@
 *
 *  The angle we get from the gyroscope is from -180 to 180, but in order to simplify the
 *  adjustion, we convert the scope from 0 to 360, clockwise represents turning right
-
+*
 ****************************************************************************************/
+#include "stm32f4xx.h"
+#include "cmath"
+
 #ifndef MOTOR_CONTROL
 #define MOTOR_CONTROL
 
 // some constrain
-#define MAX_SPEED 100   
+#define MAX_SPEED 1000   
 #define MAX_ANGLE 360
 #define MIN_ANGLE 0
 #define LEFT 0
 #define RIGHT 1
 
 // PID parameters
-#define P_DATA 10
-#define I_DATA 0.6 
-#define D_DATA 1
+#define P_DATA 10
+#define I_DATA 0.6
+#define D_DATA 1
 #define ACCEPTTED_ERROR 10
 
+/************************  useful tools  ******************************/
+
+// constrain the angle and speed
 int16_t constrain(int16_t input, int16_t min, int16_t max);
+// transform the abgle from 180 to 360  
 int16_t angleTransform(int16_t angle_ABS180);
-void Moter_Move(uint16_t angle, bool if_related, uint16_t speed);
+// get absolutely value
+uint16_t myABS(int16_t value);
+
+/**********************************************************************/
+// PID -- turn the angle
+void Motor_Move(uint16_t angle, char if_related, uint16_t speed);
+// PID -- set the speed
+void MotorSpeedPID();
+
 
 #endif // MOTOR_CONTROL
