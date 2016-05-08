2d2330a257c4efa9790d3cf77090a979252b1c84
Han ZHANG <Han ZHANG>
2016-05-09 06:31:03 +0800

    Add test project for dwm_base board. Note that the CLA project need to be cleaned after this test past.

---
+++ Codes/Test_Base_Board/Dwm1000/dwm1000_port.h
@@ -0,0 +1,86 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __DWM1000_PORT_H
+#define __DWM1000_PORT_H
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx.h"
+
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+int writetospi_serial(uint16_t headerLength,
+                             const uint8_t *headerBuffer,
+                             uint32_t bodylength,
+                             const uint8_t *bodyBuffer);
+
+int readfromspi_serial(uint16_t	headerLength,
+                              const uint8_t *headerBuffer,
+                              uint32_t readlength,
+                              uint8_t *readBuffer );
+															
+unsigned long portGetTickCnt(void);
+															
+#define portGetTickCount() 									portGetTickCnt()
+
+#define writetospi													writetospi_serial
+#define readfromspi													readfromspi_serial
+
+//SPI3 for DW1000
+#define SPI_DW1000_RCC_CLOCK								RCC_APB1Periph_SPI3															
+															
+#define SPI_DW1000_PRESCALER								SPI_BaudRatePrescaler_4 //DW的SPI 20M是极限
+#define SPI_DW1000													SPI3
+#define SPI_DW1000_AF												GPIO_AF_SPI3
+#define SPI_DW1000_CS												GPIO_Pin_4							//(407ZG)PA4-NSS
+#define SPI_DW1000_CS_GPIO									GPIOA
+#define SPI_DW1000_SCK											GPIO_Pin_10							//(407ZG)PC10-SCK
+#define SPI_DW1000_MISO											GPIO_Pin_11							//(407ZG)PC11-MISO
+#define SPI_DW1000_MOSI											GPIO_Pin_12							//(407ZG)PC12-MOSI
+#define SPI_DW1000_GPIO											GPIOC
+#define SPI_DW1000_SCK_Source								GPIO_PinSource10
+#define SPI_DW1000_MISO_Source							GPIO_PinSource11
+#define SPI_DW1000_MOSI_Source							GPIO_PinSource12
+
+#define DW1000_RSTn													GPIO_Pin_4
+#define DW1000_RSTn_GPIO										GPIOB
+#define DECARSTIRQ               					  GPIO_Pin_4
+#define DECARSTIRQ_GPIO            				  GPIOB
+#define DECARSTIRQ_EXTI           				  EXTI_Line4
+#define DECARSTIRQ_EXTI_PORT      				  EXTI_PortSourceGPIOB
+#define DECARSTIRQ_EXTI_PIN       				  EXTI_PinSource4
+#define DECARSTIRQ_EXTI_IRQn      				  EXTI4_IRQn							//(407ZG)EXTI_Line4
+
+#define DECAIRQ                 			  	  GPIO_Pin_2
+#define DECAIRQ_GPIO          							GPIOD
+#define DECAIRQ_EXTI            			 	    EXTI_Line2
+#define DECAIRQ_EXTI_PORT       			  	  EXTI_PortSourceGPIOD
+#define DECAIRQ_EXTI_PIN        			  	  EXTI_PinSource2
+#define DECAIRQ_EXTI_IRQn        			 	    EXTI2_IRQn							//(407ZG)EXTI_Line2
+
+#define port_SPI_DW1000_busy_sending()			(SPI_I2S_GetFlagStatus((SPI_DW1000),(SPI_I2S_FLAG_TXE))==(RESET))
+#define port_SPI_DW1000_no_data()						(SPI_I2S_GetFlagStatus((SPI_DW1000),(SPI_I2S_FLAG_RXNE))==(RESET))
+#define port_SPI_DW1000_send_data(x)				SPI_I2S_SendData((SPI_DW1000),(x))
+#define port_SPI_DW1000_receive_data()			SPI_I2S_ReceiveData(SPI_DW1000)
+#define port_SPI_DW1000_disable()						SPI_Cmd(SPI_DW1000,DISABLE)
+#define port_SPI_DW1000_enable()						SPI_Cmd(SPI_DW1000,ENABLE)
+#define port_SPI_DW1000_set_chip_select()		GPIO_SetBits(SPI_DW1000_CS_GPIO,SPI_DW1000_CS)
+#define port_SPI_DW1000_clear_chip_select()	GPIO_ResetBits(SPI_DW1000_CS_GPIO,SPI_DW1000_CS)
+
+ITStatus EXTI_GetITEnStatus(uint32_t x);
+#define port_GetEXT_IRQStatus()             EXTI_GetITEnStatus(DECAIRQ_EXTI_IRQn)
+#define port_DisableEXT_IRQ()               NVIC_DisableIRQ(DECAIRQ_EXTI_IRQn)
+#define port_EnableEXT_IRQ()                NVIC_EnableIRQ(DECAIRQ_EXTI_IRQn)
+#define port_CheckEXT_IRQ()                 GPIO_ReadInputDataBit(DECAIRQ_GPIO, DECAIRQ)
+
+/* Exported functions ------------------------------------------------------- */
+//DW1000
+void SPI_DW1000_ChangeRate(uint16_t scalingfactor);
+void SPI_DW1000_ConfigFastRate(uint16_t scalingfactor);
+void SPI_DW1000_set_rate_low (void); //Set SPI rate to less than 3 MHz to properly perform DW1000 initialisation.
+void SPI_DW1000_set_rate_high (void); //Set SPI rate as close to 20 MHz as possible for optimum performances.
+
+void RESET_DW1000(void);
+
+void Dwm1000_Init(void);
+
+#endif /* __DWM1000_PORT_H */
